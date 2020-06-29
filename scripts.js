6f9d700176d00f74d294104d88868011d6c1f3cc
berryboylb <phemmynesce4life@gmail.com>
2020-06-29 02:06:37 +0100

    whatever

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+ 
  //Get the button:
  mybutton = document.getElementById("myBtn");
   
