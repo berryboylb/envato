8cc2f6168ec286998aea57260d881057d5dd0dcb
berryboylb <phemmynesce4life@gmail.com>
2020-06-30 02:53:33 +0100

    whatever

--- scripts.js
+++ scripts.js
@@ -7,10 +7,10 @@ function myFunction() {
     }
   }
  
- //Get the button:
+ //button starts
  mybutton = document.getElementById("myBtn");
   
- // When the user scrolls down 20px from the top of the document, show the button
+ 
  window.onscroll = function() {scrollFunction()};
  
  function scrollFunction() {
@@ -20,8 +20,9 @@ function myFunction() {
      mybutton.style.display = "none";
    }
  }
- // When the user clicks on the button, scroll to the top of the document
+ 
  function topFunction() {
-   document.body.scrollTop = 0; // For Safari
-   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+   document.body.scrollTop = 0;
+   document.documentElement.scrollTop = 0; 
  }
+//button stops
